bd0339566147ef98b435642c88e6ca5dd362128b
ISSNET\SArchakamParankusam <Srivatsa.ArchakamParankusam@iss-shipping.com>
2023-02-21 18:37:46 +0530

    second commit

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,11 +8,11 @@ dotenv.config();
 
 const router = express.Router();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 router.route("/").get(async (req, res) => {
   try {
